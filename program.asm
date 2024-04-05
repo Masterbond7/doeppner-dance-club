77f383de27dcde057f13b286bfe810e15f3cd3e5
Wanties <wantyjunior@gmail.com>
2024-04-05 04:36:56 +0200

    live linux reaction

--- program.asm
+++ program.asm
@@ -2,42 +2,26 @@ global _start
 global msg
 global msglen
 
-%define LINUX 1
-
-; Linux specific code
-%ifdef LINUX
-    section .text
-    ; Print text to stdout (message, message length)
-    print:
-        pop rbx
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, rsp
-        add rsp, 8
-        pop rdx
-        syscall
-        push rbx
-        ret
-
-    ; Exit with code (exit code)
-    exit:
-        pop rbx
-        mov rax, 60
-        pop rdi
-        syscall
-
-
-; Windows specific code
-%elifdef WINDOWS
-%endif
-
-
-; Defining constants
-section .data
-    msg: db 0x30+3d, 0xa;db "Hello world!", 0x7, 0xa
-    ;msglen: equ $ - msg
-
 section .text
+; Print text to stdout (message, message length)
+print:
+    pop rbx
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rsp
+    add rsp, 8
+    pop rdx
+    syscall
+    push rbx
+    ret
+
+; Exit with code (exit code)
+exit:
+    pop rbx
+    mov rax, 60
+    pop rdi
+    syscall
+
 _start:
     ;push msglen
     ;push msg

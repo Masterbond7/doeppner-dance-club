4ffa1bf645f2523821354106d4ea6713e98dfe20
Masterbond7 <jamesslough4@gmail.com>
2024-04-11 10:04:51 +1200

    Code to convert String to Int

--- Sloughs_Mischief/calculator.asm
+++ Sloughs_Mischief/calculator.asm
@@ -5,6 +5,7 @@ section .data
 
 section .bss
     tbuffer resb tbuffer_len ; Buffer for text input
+    number resb 8
 
 section .text
 ; Main function
@@ -14,15 +15,58 @@ _start:
     push tbuffer_len
     call get_input
 
-    ; Print stuff
+    ; Convert input to a number
+    push number
     push tbuffer
-    push tbuffer_len
+    call conv_a2dec
+
+    ; Print stuff
+    push number
+    push 1d
     call print
 
     ; Exit
     mov rax, 60
     mov rdi, 0
     syscall
+
+; Convert ASCII to dec (*number, *char buffer)
+conv_a2dec:
+    ; Pop return address and store in r15
+    pop r15
+
+    ; Pop pointers
+    pop r9 ; *char buffer -> r9
+    pop r8 ;      *number -> r8
+
+    ; Zero byte counter & RAX (result)
+    xor r10, r10
+    xor rax, rax
+
+    .recursive:
+        ; Get a charachter
+        movzx rcx, BYTE [r9+r10] ; Move char into RCX
+        add r10, 1d              ; Move to next char
+
+        ; Exit if charachter is not a number
+        cmp rcx, 0x30 ; / If char < "0"
+        jb .exit      ; \ Exit function
+        cmp rcx, 0x39 ; / If char > "9"
+        jg .exit      ; \ Exit function
+        
+        ; Convert char to dec
+        sub rcx, 0x30  ; Char - "0"
+        imul rax, 10d  ; Multiply current result by 10
+        add rax, rcx   ; Add char to end of current result
+        jmp .recursive ; Loop until inval char
+
+    .exit:
+        ; Move result into *num
+        mov [r8], rax
+
+        ; Return to program
+        push r15 ; Put return address back
+        ret      ; Return
 
 
 ; Get input from stdin (*char buffer, # chars)

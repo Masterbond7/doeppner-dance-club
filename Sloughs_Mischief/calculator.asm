dfdcb9b6f30aa37ccef6ee8092d565bab65089b4
Masterbond7 <jamesslough4@gmail.com>
2024-04-11 08:56:37 +1200

    take and repeat user input

--- Sloughs_Mischief/calculator.asm
+++ Sloughs_Mischief/calculator.asm
@@ -1,40 +1,58 @@
 global _start
 
-section .bss
+section .data
+    tbuffer_len equ 21 ; # chars for text buffer
 
+section .bss
+    tbuffer resb tbuffer_len ; Buffer for text input
 
 section .text
+; Main function
 _start:
-    mov bx, 5d
-    mov ax, 4d
-    neg ax
-    add bx, ax
-
-    add bx, 0x30
-    push bx
-    push 1d
+    ; Get input from user
+    push tbuffer
+    push tbuffer_len
+    call get_input
 
     ; Print stuff
+    push tbuffer
+    push tbuffer_len
     call print
 
+    ; Exit
     mov rax, 60
     mov rdi, 0
     syscall
 
-; Print to stdout (message, message length)
+
+; Get input from stdin (*char buffer, # chars)
+get_input:
+    ; Pop return address and store in r15
+    pop r15
+
+    ; Move to registers and read
+    mov rax, 0 ; sys_read
+    mov rdi, 0 ; stdin
+    pop rdx    ; # of chars
+    pop rsi    ; Pointer to buffer
+    syscall    ; Call kernel
+
+    ; Return to program
+    push r15 ; Put return address back
+    ret      ; Return
+
+
+; Print to stdout (*message, message length)
 print:
     ; Pop return address and store in r15
     pop r15
 
     ; Move to registers and print
-    mov rax, 1   ; sys_write
-    mov rdi, 1   ; stdout
-    pop rdx      ; Length of the message
-    mov rsi, rsp ; Pointer to the message
-    syscall      ; Call kernel
-
-    ; Move stack pointer back past the message
-    add rsp, rdx
+    mov rax, 1 ; sys_write
+    mov rdi, 1 ; stdout
+    pop rdx    ; Length of the message
+    pop rsi    ; Pointer to buffer
+    syscall    ; Call kernel
 
     ; Return to program
     push r15 ; Put return address back

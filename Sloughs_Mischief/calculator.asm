c5649068c80ade8cb1691cd1330e2c2d6fe5b015
Masterbond7 <jamesslough4@gmail.com>
2024-04-10 18:34:49 +1200

    cool print function

--- Sloughs_Mischief/calculator.asm
+++ Sloughs_Mischief/calculator.asm
@@ -1,15 +1,41 @@
 global _start
 
-%include "./constants.asm"
+section .bss
+
 
 section .text
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, MESSAGE
-    mov rdx, MSG_LEN
-    syscall
+    mov bx, 5d
+    mov ax, 4d
+    neg ax
+    add bx, ax
+
+    add bx, 0x30
+    push bx
+    push 1d
+
+    ; Print stuff
+    call print
 
     mov rax, 60
     mov rdi, 0
     syscall
+
+; Print to stdout (message, message length)
+print:
+    ; Pop return address and store in r15
+    pop r15
+
+    ; Move to registers and print
+    mov rax, 1   ; sys_write
+    mov rdi, 1   ; stdout
+    pop rdx      ; Length of the message
+    mov rsi, rsp ; Pointer to the message
+    syscall      ; Call kernel
+
+    ; Move stack pointer back past the message
+    add rsp, rdx
+
+    ; Return to program
+    push r15 ; Put return address back
+    ret      ; Return
--- Sloughs_Mischief/constants.asm
+++
@@ -1,3 +0,0 @@
-section .data
-MESSAGE db "Hello World!", 0x0A
-MSG_LEN equ $ - MESSAGE

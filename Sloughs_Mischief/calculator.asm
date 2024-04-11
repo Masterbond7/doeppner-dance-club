4ea727403cc6ff07d264b638c56d4a060842e23a
Masterbond7 <jamesslough4@gmail.com>
2024-04-12 08:36:08 +1200

    Added int to ascii and code to multiply 2 numbers. TODO: Clear text buffer

--- Sloughs_Mischief/calculator.asm
+++ Sloughs_Mischief/calculator.asm
@@ -19,21 +19,105 @@ _start:
     push number
     push tbuffer
     call conv_a2int
+    mov r12, [number]
 
-    mov  rax, [number]
-    imul rax, 2
-    add  rax, 0x30
+    ; Get second input from user
+    push tbuffer
+    push tbuffer_len
+    call get_input
+
+    ; Convert second input to a number
+    push number
+    push tbuffer
+    call conv_a2int
+
+    ; Maths time!
+    mov rax, r12
+    mov r12, [number]
+    imul rax, r12
     mov  [number], rax
 
-    ; Print stuff
+    ; Convert number to text
+    push tbuffer
     push number
-    push 1d
+    call conv_int2a
+
+    ; Print stuff
+    push tbuffer
+    push tbuffer_len
     call print
 
     ; Exit
     mov rax, 60
     mov rdi, 0
     syscall
+
+
+; Convert int to ASCII (*number) returns on stack (len, string...)
+conv_int2a:
+    ; Pop return address and store in r15
+    pop r15
+
+    ; Pop pointers
+    pop r9 ;      *number -> r9
+    pop r8 ; *char buffer -> r8
+
+    ; Zero char counter
+    xor r10, r10
+
+    ; Push newline to the stack
+    push 0x0A ; Push newline
+    inc r10   ; Inc char counter
+
+    ; Move 10d to r11 for division
+    mov r11, 10d
+
+    .recursive:
+        ; Number division
+        mov rax, [r9] ; Move num into RAX
+        xor rdx, rdx  ; Zero RDX (remainder)
+        div r11       ; Divide RDX:RAX by 10
+        mov [r9], rax ; Move quotient to num
+
+        ; Convert num to char
+        add rdx, 0x30 ; Add "0" to RDX (remainder)
+        push dx       ; Push charachter to the stack
+        inc r10       ; Inc char counter
+
+        ; If quotient is 0, unstack the stack
+        cmp rax, 0d
+        je .s_unstack
+
+        ; Otherwise jump back to top
+        jmp .recursive
+
+    .s_unstack:
+        ; Zero byte counter
+        xor r11, r11
+
+        ; Go to the unstacking loop
+        jmp .unstack
+    
+    .unstack:
+        ; Pop from stack into text buffer
+        pop ax           ; Pop char into AX
+        mov [r8+r11], al ; Move first byte of AX to t_buffer
+
+        ; Increase byte counter
+        inc r11
+
+        ; Exit if byte counter = char counter
+        cmp r11, r10
+        je .exit
+
+        ; Otherwise jump to top of loop
+        jmp .unstack
+
+    .exit:
+        ; Return to program
+        push r15 ; Put return address back
+        ret      ; Return
+
 
 ; Convert ASCII to int (*number, *char buffer)
 conv_a2int:

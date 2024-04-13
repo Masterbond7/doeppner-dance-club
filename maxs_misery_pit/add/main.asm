75ee760e339e1f28d78861039055b7d3d5a5563f
Grapes42 <f.inggrapes@gmail.com>
2024-04-13 22:33:09 +1200

    made a print macro

--- maxs_misery_pit/add/main.asm
+++ maxs_misery_pit/add/main.asm
@@ -1,10 +1,25 @@
 global _start
 
-section .data
-    prompt db "Num 1: "
-    prompt_len equ $ - prompt
+%macro print 1
+        mov rax, 1 ; sys_write
+        mov rdi, 1 ; stdout
+
+        mov rdx, %%message_len
+        mov rsi, %%message
+
+        syscall
+        jmp %%over
+
+    %%message: db %1 ; define bytes for string
+    %%message_len: equ $ - %%message ; get length of string
+    %%over:
+%endmacro
 
 section .text
 
 _start:
-    
+    print "hello"
+
+    mov rax, 60 ; exit command
+    mov rdi, 0 ; valid exit code
+    syscall
